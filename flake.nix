d3d7e9fd266ef7931c28e437d23e928defbaf89d
caKambourides <charalaboskambourides@gmail.com>
2025-09-01 18:56:10 +0300

    fixed

--- flake.nix
+++ flake.nix
@@ -3,18 +3,62 @@
 
   inputs = {
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
-  outputs = { self, nixpkgs, ...  }:
-  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  outputs = { self, nixpkgs,
+    rust-overlay, ...  }:
+    let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [rust-overlay.overlays.default];
+    };
+    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
   in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
+    devShells.${system} = {
+      
+bevy = pkgs.mkShell {
+      packages = [
+        toolchain pkgs.rust-analyzer-unwrapped
+
+        #rust
+        pkgs.cargo
+        pkgs.rustc
+        # pkgs.rust-analyzer
+        pkgs.rustfmt
+
+        # If the dependencies need system libs, you usually need pkg-config + the lib
+        pkgs.pkg-config
+        pkgs.openssl
+
+        # bev
+        # y https:/
+        # /github.com/bevyengine/bevy/blob/e67cfdf82b5726db4d449e9af31b865a5324aa19/docs/linux_dependencies.md#nix
+        pkgs.udev
+        pkgs.alsa-lib
+        pkgs.vulkan-loader
+        pkgs.libxkbcommon
+        pkgs.wayland
+        #bevy optimized
+        
+        
+        #env = {
+        #  RUST_BACKTRACE = "full";
+        #};
+            ];
+      RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+
+    };
+
+typescript = pkgs.mkShell {
       packages = with pkgs; [
+        #ts
         nodejs
         nodePackages.prettier
         nodePackages.eslint
         nodePackages.typescript-language-server
-      ];
-    };
-
+];
+    };    
   };
+};
 }
